daa7a3c587945e8fd7fb90283c2939c378f0cf85
Aniket <aniketsinare.dev@gmail.com>
2026-05-18 23:07:48 +0530

    18 may NUmbered Pattern

--- Striver/Pattern1.py
+++ Striver/Pattern1.py
@@ -36,3 +36,40 @@ print("Right Angle Triangle")
 print(RightAngleTrianglePattern(5))
 
 
+# Pattern - 3: Right-Angled Number Pyramid
+
+# Problem Statement: Given an integer N, print the following pattern : 
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+def NumberedPattern(n):
+    for i in range(n):
+        for j in range(i):
+            print(j+1, " ", end="")
+        print()
+
+print("Numbered Pattern")
+print(NumberedPattern(6))
+
+
+# Pattern - 4: Right-Angled Number Pyramid - II
+
+# Problem Statement: Given an integer N, print the following pattern : 
+
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+def NumberedPatternTwo(n):
+    for i in range(n):
+        for j in range(i):
+            print(i, " ", end="" )
+        print()
+
+print(NumberedPatternTwo(6))
